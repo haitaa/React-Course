7cc9fa65f6554e6590ef9e1d8a07219d9de0f3e8
Mustafa Haita <mustafahaitaa@gmail.com>
2024-06-13 21:44:52 +0300

    Starting Project

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,28 @@
+import { CORE_CONCEPTS } from "./data";
+import { Header } from "./components/Headers";
+import { CoreConcept } from "./components/CoreConcepts";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
