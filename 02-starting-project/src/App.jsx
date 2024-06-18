91991dd7ff9546c4c632bef35caadde91ebe1cc8
Mustafa Haita <mustafahaitaa@gmail.com>
2024-06-18 15:20:51 +0300

    Outputting Results in a List & Deriving More Values

--- 02-starting-project/src/App.jsx
+++ 02-starting-project/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,
       };
     });
   }
@@ -24,7 +24,7 @@ function App() {
     <>
       <Header />
       <InputTable onChange={handleChange} userInput={userInput} />
-      <Results input={{ userInput }} />
+      <Results input={userInput} />
     </>
   );
 }
